5be82a0d86d42145401cb28ba1ce986685d62b0c
TabassumaMehenaz <tabassumamehenaz@gmail.com>
2025-06-15 11:03:20 +0600

    source(Admin)

---
+++ AdminNavigation.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Admins;
+
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author hello
+ */
+public class AdminNavigation {
+        
+    public static JFrame previousFrame = null; // Static variable to store the previous frame
+}
