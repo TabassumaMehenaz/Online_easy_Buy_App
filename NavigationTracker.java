cfd83f0a52d354eaeafdc93e3aa590c9cfcbc544
TabassumaMehenaz <tabassumamehenaz@gmail.com>
2025-06-15 11:10:53 +0600

    source(User)

---
+++ NavigationTracker.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package User;
+
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author HP
+ */
+public class NavigationTracker {
+    public static JFrame previousFrame = null; // Static variable to store the previous frame
+}
