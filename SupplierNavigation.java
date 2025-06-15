73596e931b08adddb3453c33a69b393eb1e2dacb
TabassumaMehenaz <tabassumamehenaz@gmail.com>
2025-06-15 11:06:49 +0600

    source(JDBC(insert.form and .java) and Supplier )

---
+++ SupplierNavigation.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Suppliers;
+
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author hello
+ */
+public class SupplierNavigation {
+          public static JFrame previousFrame = null; // Static variable to store the previous frame
+}
